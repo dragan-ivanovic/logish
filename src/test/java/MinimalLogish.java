26cb66e50e570610b2e2c447eb41c24b4ec9b652
Dragan Ivanovic <idragan@mac.com>
2019-12-08 17:29:12 +0100

    Documentation.

--- src/test/java/MinimalLogish.java
+++ src/test/java/MinimalLogish.java
@@ -1,13 +1,13 @@
 import static org.cellx.logish.Logish.run;
-import static org.cellx.logish.Logish.Goal.*;
-import io.vavr.collection.Stream;
+        import static org.cellx.logish.Logish.Goal.*;
+        import io.vavr.collection.Stream;
 
 public class MinimalLogish {
 
- public static void main(String[] argv) {
-   final Stream<Object> result = run(q -> unify(q, "World"));
-   for (final Object o: result) {
-     System.out.println("Hello, " + o + "!");
-   }
- }
+    public static void main(String[] argv) {
+        final Stream<Object> result = run(q -> unify(q, "World"));
+        for (final Object o: result) {
+            System.out.println("Hello, " + o + "!");
+        }
+    }
 }
